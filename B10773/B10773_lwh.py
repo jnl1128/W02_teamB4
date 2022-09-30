4c74a185b17dfe02a6a311a2f66f89d6e3f8505a
wnhlee <2wheeh@gmail.com>
2022-09-30 20:27:29 +0900

    제로 117576kb 140ms

---
+++ B10773/B10773_lwh.py
@@ -0,0 +1,34 @@
+import sys
+sys.stdin = open('B10773/input.txt', 'r')
+input = sys.stdin.readline
+
+class Stack:
+    def __init__(self, capacity):
+        self.stk = [None] * capacity
+        self.capacity = capacity
+        self.ptr = 0
+    
+    def pop(self):
+        self.ptr -= 1
+        return self.stk[self.ptr]
+    
+    def push(self, value):
+        self.stk[self.ptr] = value
+        self.ptr += 1
+    def sum(self):
+        sum = 0
+        for idx in range(self.ptr):
+            sum += self.stk[idx]
+        return sum
+
+K = int(input())
+integers = [int(input()) for _ in range(K)]
+account = Stack(K)
+for integer in integers:
+    if integer == 0:
+        account.pop()
+    else:
+        account.push(integer)
+# print(sum(account))
+# print(account)
+print(account.sum())
