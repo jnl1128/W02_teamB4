9aecc84f6f03cf07654a381f083b1576ef6e0122
wnhlee <2wheeh@gmail.com>
2022-09-30 21:54:22 +0900

    괄호 114488kb 120ms

--- B10773/B10773_lwh.py
+++ B10773/B10773_lwh.py
@@ -15,6 +15,7 @@ class Stack:
     def push(self, value):
         self.stk[self.ptr] = value
         self.ptr += 1
+        
     def sum(self):
         sum = 0
         for idx in range(self.ptr):
