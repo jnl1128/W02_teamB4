80b783af612b6c11b1fbc3b8ca9ad87e91750242
narin <nalynnshinhwa1128@gmail.com>
2022-09-30 15:15:11 +0900

    나무자르기 return으로 while문  탈출코드 추가 148396kb 2808ms

--- B2805/B2805_jnl.py
+++ B2805/B2805_jnl.py
@@ -6,19 +6,21 @@ N, M = map(int, input().split(' '))
 arr = list(map(int, input().split(' ')))
 
 def binary():
-    start, end = 1, max(arr)
+    start, end = 1, max(arr) 
     result = 0
     while start <= end:
-        mid = (start+end)//2
+        mid = (start+end)//2 # 10
         total = 0
         for x in arr:
             if x > mid:
                 total += x-mid
         if total < M:
-            end = mid -1
-        else:
+            end = mid -1 #9
+        elif total > M:
             result = mid
             start = mid + 1
+        else:
+            return mid
     return result
 
 print(binary())
