109995bbae925ecda5f4ee300888494ea45dc8b3
narin <nalynnshinhwa1128@gmail.com>
2022-10-04 13:09:55 +0900

    크게 만들기 39328kb 476ms

--- B2812/B2812_jnl.py
+++ B2812/B2812_jnl.py
@@ -8,14 +8,11 @@ for idx, num in enumerate(arr):
     pos = len(result)
     
     while stack and stack[-1] < num:
-        a = stack.pop()
-        # print('기준: ', num, 'pop', a)
+        stack.pop()
     stack.append(num)
-    # print('stack:', stack)
-
+    
     if idx == (N-(N-K)+pos):
-        # print('선택의 순간', idx)
+        # 선택의 순간
         result += str(stack.popleft())
-        # result.append(str(stack.popleft()))
 print(f'{result}')
 
